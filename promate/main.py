a2bb81bc363df5cd6b66ff61bd73dc0fe0a7c6e8
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-09-20 00:41:22 -0700

    Update files

--- promate/main.py
+++ promate/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
+from promate.routers.employee import router as employee_router
+
 app = FastAPI()
+
+app.include_router(employee_router, prefix="/api/v1/employees")
 
 
 @app.get("/")
--- promate/routers/employee.py
+++ promate/routers/employee.py
@@ -1,1 +1,15 @@
-from promate.models import Employee
+from fastapi import APIRouter
+
+from promate.models.employee import Employee
+
+router = APIRouter()
+
+
+@router.get("/")
+async def health_check():
+    return "The employees service is running!"
+
+
+@router.get("/employees/{employee_id}", response_model=Employee)
+async def get_employee(employee_id: int):
+    return {"employee_id": employee_id}
